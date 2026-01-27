e4d3619a034009954a5a221c39ae31a4ae2fb57d
airvivu <airvivu@gmail.com>
2026-01-27 12:51:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ const PORT = process.env.PORT || 3000;
 // Add as many origins as you like; each maps to exactly one asset.
 const ORIGIN_ASSETS = {
   "https://seishinyoga-arh0fkd7duceeseq.z01.azurefd.net": {
-    htmlFile: "asset1.html",
-    //htmlFile: "asset2.html",
-    audioUrl: "https://audio.jukehost.co.uk/DyUy2se4Zy5Jn86qOojhJ6ttzee3XVu0",
-    //audioUrl: "https://audio.jukehost.co.uk/5g8FN9boWZNIEO9XW66sXMafW4rOaHDR",
+    //htmlFile: "asset1.html",
+    htmlFile: "asset2.html",
+    //audioUrl: "https://audio.jukehost.co.uk/DyUy2se4Zy5Jn86qOojhJ6ttzee3XVu0",
+    audioUrl: "https://audio.jukehost.co.uk/5g8FN9boWZNIEO9XW66sXMafW4rOaHDR",
   },
   "https://sanyoga.life": {
     htmlFile: "asset2.html",
