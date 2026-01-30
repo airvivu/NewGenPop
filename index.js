8bf09e280c85dee68cd475891c7f8e1e7cba88b1
airvivu <airvivu@gmail.com>
2026-01-30 11:30:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const ORIGIN_ASSETS = {
   "https://seishinyoga-arh0fkd7duceeseq.z01.azurefd.net": {
     htmlFile: "asset1.html",
     //htmlFile: "asset2.html",
-    audioUrl: "https://audio.jukehost.co.uk/h7eaiT6O5GkEAwKk2qf19Cu34ugDKjYq",
+    audioUrl: "https://audio.jukehost.co.uk/ECrqTh2pOeuY13POwssZj1PaRGMyKxse",
     //audioUrl: "https://audio.jukehost.co.uk/FwdgK560zxjOhlx7tpcIwCPfiUAbYSRk",
   },
   "https://sanyoga.life": {
